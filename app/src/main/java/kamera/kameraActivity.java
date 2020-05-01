7efae63665d9a10f31dc1d4a9b52ceea52e57e11
unknown <github_mzulfaas>
2020-05-01 15:09:32 +0700

    mau mulai kkerjain bottom sheet

---
+++ app/src/main/java/kamera/kameraActivity.java
@@ -0,0 +1,16 @@
+package kamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.skincareapp.R;
+
+public class kameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kamera);
+
+    }
+}
